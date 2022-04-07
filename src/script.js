a03d343d50a0ec9bf0d0de441d099b61efa19b4a
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2022-04-07 13:29:17 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,40 @@
+// Side Navigation Code
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let navBar = document.querySelector(".navbar");
+
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+};
+
+cancelBtn.onclick = function () {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+};
+
+// Sticky Navigation Code and Scroll to top Button Code
+const nav = document.querySelector("nav");
+const scrollButton = document.getElementById("topBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+        scrollButton.style.display = "block";
+    } else {
+        nav.classList.remove("sticky");
+        scrollButton.style.display = "none";
+    }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
